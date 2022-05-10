2ff2122ea21fd0ad4cf580cf6302953c6d5ef1f3
Paulo Roberto da Silva <paulorsilva92@outlook.com>
2022-05-10 17:22:59 -0300

    Ajustes na view para mudar a direção da animação e colocar o nome da cidade e um background semi transparent nos textos de temperatura e hora

--- app/src/main/java/com/example/namariaanimation/Namaria.kt
+++ app/src/main/java/com/example/namariaanimation/Namaria.kt
@@ -1,14 +1,11 @@
 package com.example.namariaanimation
 
-import android.animation.ObjectAnimator
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
-import android.widget.TextView
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.Group
 
 
 class Namaria @JvmOverloads constructor(
@@ -18,37 +15,37 @@ class Namaria @JvmOverloads constructor(
     defStyleRes: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr, defStyleRes) {
 
-    private val leftTranslation = (-500).toFloat()
-    private val rightTranslation = 500.toFloat()
+    private val leftTranslation = 500.toFloat()
+    private val rightTranslation = (-500).toFloat()
     private val animationDuration = 1200.toLong()
     private val animationInterval = 3000.toLong()
 
-    private val hourText: TextView by viewProvider(R.id.hour_text)
+    private val hourText: AppCompatTextView by viewProvider(R.id.hour_text)
     private val weatherImage: AppCompatImageView by viewProvider(R.id.weather_image)
     private val temperatureText: AppCompatTextView by viewProvider(R.id.temperature_text)
 
     init {
         inflate(context, R.layout.namaria_layout, this)
+        hourText.translationX = leftTranslation
         setupHourTimer()
     }
 
     private fun setupHourTimer() {
         postDelayed({
-            hourText.visibility = View.VISIBLE
 
             weatherImage.animateTranslationX(rightTranslation)
             temperatureText.animateTranslationX(rightTranslation)
 
             hourText.translationX = leftTranslation
             hourText.animateTranslationX(0f)
-            setupWeatherTimer()
 
+            setupWeatherTimer()
         }, animationInterval)
     }
 
     private fun setupWeatherTimer() {
         postDelayed({
-            hourText.visibility = View.VISIBLE
+
             weatherImage.translationX = leftTranslation
             temperatureText.translationX = leftTranslation
 
